57708acf46d1cfad2fe9d1a6c351a615c479a959
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-05-26 20:50:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28 MB (99.84%)

---
+++ 0169-majority-element/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate=nums[0],count=1;
+        int n=nums.size();
+        for(int i =1;i<n;i++){
+            if(candidate!=nums[i]){
+                count--;
+            } else {
+                count++;
+            }
+            if(count==0){
+                candidate=nums[i];
+                count=1;
+            }
+        }
+        return candidate;
+    }
+};
