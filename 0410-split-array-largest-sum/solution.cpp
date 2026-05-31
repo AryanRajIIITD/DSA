e95859295b8e2cfd1aa75f89a633dbc1a2b7c391
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-05-31 16:57:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.4 MB (97.85%)

--- 0410-split-array-largest-sum/solution.cpp
+++ 0410-split-array-largest-sum/solution.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
-    int can(vector<int>& nums,int sum){
-        int splits=1;
+    int sum(vector<int>&nums,int sums){
+        int split=1;
         long long totalsum=0;
         for(int i=0;i<nums.size();i++){
-            if(totalsum+nums[i]<=sum){
+            if(nums[i]+totalsum<=sums){
                 
                 totalsum+=nums[i];
+
             } else {
-                splits++;
+                split++;
                 totalsum=nums[i];
             }
         }
-        return splits;
+        return split;
     }
     
-    
-    
     int splitArray(vector<int>& nums, int k) {
-        int n=nums.size();
-        int maxi=*max_element(nums.begin(),nums.end());
-        if(k>n) return -1;
-        int l=maxi;
-        int h=0;
+        int l=*max_element(nums.begin(),nums.end());
+        int h=0,a=0;
         for(auto i:nums){
             h+=i;
         }
         while(l<=h){
             int mid=(l+h)/2;
-            int a=can(nums,mid);
-            if(a>k){
+            int ans=sum(nums,mid);
+            if(ans>k){
+                
                 l=mid+1;
             } else {
                 h=mid-1;
             }
-
         }
         return l;
     }
