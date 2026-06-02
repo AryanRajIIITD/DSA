64ba0018d600e81cc150ec3b2183f450d6b6b0f6
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-06-02 19:07:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 95 MB (80.00%)

--- 0004-median-of-two-sorted-arrays/solution.cpp
+++ 0004-median-of-two-sorted-arrays/solution.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        int i=0,j=0;
-        vector<int> arr;
-        while(i<n && j< m){
+        int n = nums1.size();
+        int m = nums2.size();
+        if (n > m) return findMedianSortedArrays(nums2, nums1);
+        
+        int low = 0, high = n;
+        int left = (n + m + 1) / 2;
+        int total_length = n + m;
+        
+        while (low <= high) { 
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
             
-            if(nums1[i]>nums2[j]){
-                arr.push_back(nums2[j]);
-                j++;
-            } else {
-                arr.push_back(nums1[i]);
-                i++;
+            int l1 = INT_MIN, l2 = INT_MIN;
+            int r1 = INT_MAX, r2 = INT_MAX;
+            
+            if (mid1 < n) r1 = nums1[mid1];
+            if (mid2 < m) r2 = nums2[mid2];
+            if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+            
+            if (l1 <= r2 && l2 <= r1) {
+                
+                if (total_length % 2 == 1) {
+                    return max(l1, l2);
+                }
+                return ((double)max(l1, l2) + (double)min(r1, r2)) / 2.0;
+            } 
+            else if (l1 > r2) {
+                high = mid1 - 1;
+            } 
+            else {
+                low = mid1 + 1;
             }
         }
-        while(j<m){
-            arr.push_back(nums2[j]);
-            j++;
-        }
-        while(i<n){
-            arr.push_back(nums1[i]);
-            i++;
-        }
-        int l=arr.size();
-        double median;
-        int a=l/2;
-        
-        if(l%2==0){
-            median=(arr[a-1]+arr[a])/2.0;
-        } else {
-            median=arr[a];
-        }
-        return median;
+        return 0.0;
     }
 };
