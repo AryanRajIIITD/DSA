f5d6472a693e6120cf8de1780ef6cfa3ae633620
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-06-18 13:42:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (55.29%)

--- 1078-remove-outermost-parentheses/solution.cpp
+++ 1078-remove-outermost-parentheses/solution.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int opened=0;
-        string st="";
-        for(char c:s){
-            if(c=='('){
-                if(opened>0){
-                    st+=c;
+        int n=s.size();
+        int o=0;
+        string cp;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                o++;
+                if(o>1){
+                    cp+=s[i];
                 }
-                opened++;
             } else {
-                opened--;
-                if(opened>0){
-                    st+=c;
+                o--;
+                if(o>0){
+                    cp+=s[i];
                 }
             }
-            
+        
         }
-        return st;
+        return cp;
     }
 };
