b60e3d596daf6e0c986a528ea1192c9b1260bfe8
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-06-19 08:59:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (82.29%)

---
+++ 0008-string-to-integer-atoi/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        int i = 0;
+        int sign = 1;
+        long total = 0;
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+      
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+        while (i < n && s[i] >= '0' && s[i] <= '9') {
+            int digit = s[i] - '0';
+            total = (total * 10) + digit;
+            if (sign == 1 && total > INT_MAX) return INT_MAX;
+            if (sign == -1 && -total < INT_MIN) return INT_MIN;
+            
+            i++;
+        }
+
+        return total * sign;
+    }
+};
+
