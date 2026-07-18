679732207e96b7137a005b6c35f578dc402fb1c8
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-07-18 18:16:52 +0000

    Sync LeetCode submission Runtime - 48 ms (11.18%), Memory - 49.6 MB (8.96%)

--- 0039-combination-sum/solution.cpp
+++ 0039-combination-sum/solution.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-    void p(vector<int>&c,int n,int i,vector<int>&d,vector<vector<int>>&r,int sum,int t){
+    void p(vector<int> c,int n, int i, int sum,vector<int>&d,vector<vector<int>>&r,int t){
         if(i==n){
             if(sum==t){
                 r.push_back(d);
             }
             return;
         }
-        p(c,n,i+1,d,r,sum,t);
+        p(c,n,i+1,sum,d,r,t);
         if(sum+c[i]<=t){
             d.push_back(c[i]);
             sum+=c[i];
-            p(c,n,i,d,r,sum,t);
+            p(c,n,i,sum,d,r,t);
             d.pop_back();
             sum-=c[i];
-            
         }
         return;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         int n=candidates.size();
-        int i=0;
-        int sum=0;
+        int i=0,sum=0;
         vector<int> d;
         vector<vector<int>> r;
-        p(candidates,n,i,d,r,sum,target);
+        p(candidates,n,i,sum,d,r,target);
         return r;
+        
     }
 };
