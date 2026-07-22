7ca7b8d725d56e4363a7ff5668e3128eb42aee35
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-07-22 12:58:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.5 MB (89.10%)

--- 0046-permutations/solution.cpp
+++ 0046-permutations/solution.cpp
@@ -5,19 +5,15 @@ public:
             r.push_back(nums);
             return;
         }
-        for(int j=i;j<nums.size();j++){
-           
+        for(int j=i;j<n;j++){
             swap(nums[j],nums[i]);
             p(nums,i+1,n,r);
-            swap(nums[i],nums[j]);
-            
-            
+            swap(nums[j],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        int n= nums.size();
         int i=0;
-        
+        int n=nums.size();
         vector<vector<int>> r;
         p(nums,i,n,r);
         return r;
