645f99904c233187197fa145055f396da6ec9eff
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-08-01 20:53:49 +0000

    Sync LeetCode submission Runtime - 61 ms (72.16%), Memory - 43.9 MB (54.94%)

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* ans=nullptr;
+    int f(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root==nullptr) return 0;
+        int left=f(root->left,p,q);
+        int right=f(root->right,p,q);
+        int self=0;
+        if(root==p || root==q) self=1;
+        int total=self+left+right;
+        if(total==2 && ans==nullptr) ans=root;
+        return total;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        f(root,p,q);
+        return ans;
+    }
+};
