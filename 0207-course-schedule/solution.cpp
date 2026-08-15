c99d2e9d62f93c2e6cff32e70a2163f67e77c68f
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-08-15 06:46:34 +0000

    Sync LeetCode submission Runtime - 8 ms (34.54%), Memory - 19.6 MB (44.78%)

---
+++ 0207-course-schedule/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& p) {
+        queue<int> q;
+        vector<int> id(V);
+        vector<int> topo;
+        vector<int> adj[V];
+        for(auto it:p){
+            adj[it[0]].push_back(it[1]);
+        }
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                id[it]++;
+            }
+        }
+        for(int i=0;i<V;i++){
+            if(id[i]==0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int  node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                id[it]--;
+                if(id[it]==0) q.push(it);
+            }
+        }
+        if(topo.size()==V) return true;
+        return false;
+    }
+};
