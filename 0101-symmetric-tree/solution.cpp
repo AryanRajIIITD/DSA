c2bfb84765534b3b16eb67ebaed4bdb5cea68cca
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-08-19 18:30:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.6 MB (40.87%)

--- 0101-symmetric-tree/solution.cpp
+++ 0101-symmetric-tree/solution.cpp
@@ -12,8 +12,8 @@
 class Solution {
 public:
     bool p(TreeNode* r1, TreeNode* r2){
-        if(r1==nullptr && r2==nullptr) return true;
-        if(r1==nullptr || r2==nullptr) return false;
+        
+        if(r1==nullptr || r2==nullptr) return (r1==r2);
         if(r1->val!=r2->val) return false;
         bool a=p(r1->left,r2->right);
         bool b=p(r1->right,r2->left);
