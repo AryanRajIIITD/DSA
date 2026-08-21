1811f1435535a7c0cfd602452db28abfa3e26935
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-08-21 13:22:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.5 MB (98.14%)

--- 0075-sort-colors/solution.cpp
+++ 0075-sort-colors/solution.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int mid=0,low=0,high=n-1;
+        for(int i=0;i<n;i++){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            } else if(nums[mid]==1){
+                mid++;
+            } else {
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
     }
 };
