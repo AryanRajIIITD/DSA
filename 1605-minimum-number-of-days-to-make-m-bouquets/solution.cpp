f1224ac119466b31b96917a632145d40fdda5c8d
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-08-25 13:41:49 +0000

    Sync LeetCode submission Runtime - 10 ms (99.53%), Memory - 70.6 MB (12.30%)

--- 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
-    long long totald(vector<int>& bloomDay, int d, int k) {
-        long long bouquets = 0;
-        int consecutive_flowers = 0;
-        int n = bloomDay.size();
-        
-        for (int i = 0; i < n; i++) {
-            
-            if (bloomDay[i] <= d) {
-                consecutive_flowers++;
-                
-                if (consecutive_flowers == k) {
-                    bouquets++;
-                    consecutive_flowers = 0;
+    long long totaldays(vector<int>&bloomDay,int day,int k){
+        long long bouquet=0;
+        int cday=0;
+        for(int i=0;i<bloomDay.size();i++){
+            if(bloomDay[i]<=day){
+                cday++;
+                if(cday==k){
+                    bouquet++;
+                    cday=0;
                 }
             } else {
-                consecutive_flowers = 0;
+                cday=0;
             }
+            
         }
-        return bouquets;
+        return bouquet;
     }
-
     int minDays(vector<int>& bloomDay, int m, int k) {
-        if ((long long)m * k > bloomDay.size()) {
-            return -1;
-        }
-
-        int maxi = *max_element(bloomDay.begin(), bloomDay.end());
-        int l = 1, h = maxi, a = -1;
-
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            long long days = totald(bloomDay, mid, k);
-
-            if (days >= m) {
-                a = mid;
-                h = mid - 1;
+        int maxi=*max_element(bloomDay.begin(),bloomDay.end());
+        int mini=*min_element(bloomDay.begin(),bloomDay.end());
+        int low=mini,high=maxi,ans=-1;
+        if((long long)m*k>bloomDay.size()) return ans;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            long long bouquet=totaldays(bloomDay,mid,k);
+            if(bouquet>=m){
+                ans=mid;
+                high=mid-1;
             } else {
-                l = mid + 1;
+                low=mid+1;
             }
         }
-        return a;
+        return ans;
     }
 };
