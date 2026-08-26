80fbd4829415439f36bb5570c555cd4b4ec0697d
AryanRajIIITD <162334178+AryanRajIIITD@users.noreply.github.com>
2026-08-27 04:21:30 +0530

    Time: 210 ms (7.51%) | Memory: 22.6 MB (10.59%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int jump(vector<int>& nums) {
-        int l=0,r=0;
-        int count=0;
-        while(r<nums.size()-1){ 
-            int farthest=0;
-            for(int j=l;j<=r;j++){
-                farthest=max(farthest,j+nums[j]);
+    int canjump(vector<int>&nums,vector<int>&dp,int i){
+        if(i>=nums.size()-1) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int mini=1e9;
+        for(int j=1;j<=nums[i];j++){
+            int jumps=canjump(nums,dp,i+j);
+            if(jumps!=1e9){ 
+                mini=min(mini,1+jumps);
             }
-            l=r+1;
-            r=farthest;
-            count+=1;
         }
-        return count;
+        return dp[i]=mini;
+    }
+    int jump(vector<int>& nums) {
+        vector<int>dp(nums.size()+1,-1);
+        return canjump(nums,dp,0);
     }
 };
